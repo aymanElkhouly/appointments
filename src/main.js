11c4ee7fd424673f482c16c3d9f070eb561524f6
aymanElkhouly <elkhouly.ayman@outlook.com>
2020-07-29 04:26:57 +0200

    Add Bootstrap & Vuex & Axios & images

--- src/main.js
+++ src/main.js
@@ -2,6 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Vuex from 'vuex'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+let modules = [Vuex,axios,VueAxios,BootstrapVue,IconsPlugin];
+modules.forEach((module) => Vue.use(module));
+//Vue.use(Vuex,axios,VueAxios,BootstrapVue,IconsPlugin);
 
 Vue.config.productionTip = false
 
